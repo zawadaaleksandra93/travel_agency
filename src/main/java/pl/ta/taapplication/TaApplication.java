3193a5f749a2b3c9d576d756e8f301484c81fbf2
zawadaaleksandra93 <zawadaaleksandra93@users.noreply.github.com>
2020-10-25 13:20:10 +0100

    TA-1 init project

---
+++ src/main/java/pl/ta/taapplication/TaApplication.java
@@ -0,0 +1,13 @@
+package pl.ta.taapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/ta/taapplication/TaApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.ta.taapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
